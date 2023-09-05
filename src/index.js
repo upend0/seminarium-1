18c44afa2403d2e2a96ce8cfd3b9380162e62e14
Maddelen Hedenstrom <mh222vu@student.lnu.se>
2023-09-05 10:36:44 +0200

    Add other functions

--- src/index.js
+++ src/index.js
@@ -2,3 +2,37 @@ function add (numberOne, numberTwo) {
   return console.log((numberOne + numberTwo))
 }
 add(2, 3)
+
+function subtract (numberOne, numberTwo) {
+  return console.log((numberOne - numberTwo))
+}
+subtract(2, 3)
+
+function multiply (numberOne, numberTwo) {
+  return console.log((numberOne * numberTwo))
+}
+multiply(2, 3)
+
+function divide (numberOne, numberTwo) {
+  return console.log((numberOne / numberTwo))
+}
+divide(2, 3)
+
+let firstNumber = Number(process.argv.slice(2, 3))
+let action = process.argv.slice(3, 4).toString()
+let secondNumber = Number(process.argv.slice(4, 5))
+
+// add(firstNumber, secondNumber) 
+console.log('TEST', action)
+if (action === '+') {
+  add(firstNumber, secondNumber)
+  
+} else if (action === '-') {
+  subtract(firstNumber, secondNumber)
+} else if (action === '*') {
+  multiply(firstNumber, secondNumber)
+} else if (action === '/') {
+  divide(firstNumber, secondNumber)
+} else {
+  console.log('Enter a valid action')
+}
